3ddd4a8aefbe22b5fb1b58892ad9dfff3a9d900a
Tyler Kap <tylerkap55@gmail.com>
2025-04-09 16:34:08 -0600

    unit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './comonents/timesheet/timesheet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentsComponent
+    DepartmentsComponent,
+    TimesheetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/timesheet/timesheet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timesheet',
+  standalone: false,
+  templateUrl: './timesheet.component.html',
+  styleUrl: './timesheet.component.scss'
+})
+export class TimesheetComponent {
+
+}
