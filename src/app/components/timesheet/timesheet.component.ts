5a317330e59a8da7aaf34ce1d0c403baf0da6360
Tyler Kap <tylerkap55@gmail.com>
2025-04-15 18:17:27 -0600

    unit 6 - routing complete

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from '../../interfaces/department';
+import { DepartmentsService } from '../../services/departments.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -7,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './timesheet.component.scss'
 })
 export class TimesheetComponent {
+  
+  departments: Department[] | undefined;
+  department: Department | undefined;
+  
+  
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService
+  ) {}
 
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+  }
 }
