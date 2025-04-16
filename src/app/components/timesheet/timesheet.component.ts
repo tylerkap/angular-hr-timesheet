df96da16d359447c7697b91d9ef7c78a3a932bf5
Tyler Kap <tylerkap55@gmail.com>
2025-04-15 18:29:56 -0600

    unit 7 - part-2 complete

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Department } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
 import { FormControl } from '@angular/forms';
+import { Employee } from '../../interfaces/employee';
 
 @Component({
   selector: 'app-timesheet',
@@ -15,6 +16,8 @@ export class TimesheetComponent {
   departments: Department[] | undefined;
   department: Department | undefined;
   employeeNameFC = new FormControl('');
+  employees: Employee[] = [];
+  employeeId = 0;
   
   
   constructor(
@@ -26,4 +29,19 @@ export class TimesheetComponent {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
   }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+        this.employeeId++;
+
+        this.employees.push({
+            id: this.employeeId.toString(),
+            departmentId: this.department?.id,
+            name: this.employeeNameFC.value,
+            payRate: Math.floor(Math.random() * 50) + 50,
+        });
+
+        this.employeeNameFC.setValue('');
+    }
+}
 }
