2858876ec7ed62cfb80c7e0495497fb0978443ce
Tyler Kap <tylerkap55@gmail.com>
2025-05-06 14:57:46 -0600

    Unit 14 finished - timesheet app complete

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -87,7 +87,11 @@ export class TimesheetComponent {
         + employee.thursday + employee.friday + employee.saturday + employee.sunday;
   }
 
-  deleteEmployee(index: number): void {
+  deleteEmployee(employee: Employee, index: number): void {
+    if (employee.id) {
+        this.employeeService.deleteEmployeeHours(employee);
+    }
+
     this.employees.splice(index, 1);
   }
 
